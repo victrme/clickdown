0c6f91096a589b2410148e296dcf099d4a1767fc
Victor Azevedo <victor.azevedo@mail.com>
2024-06-28 08:25:45 +0200

    Added JSDoc to onclickdown

--- src/types.ts
+++ src/types.ts
@@ -1,17 +1,47 @@
 declare global {
+	type PointerMouseKeyboard = PointerEvent | MouseEvent | KeyboardEvent
+
 	interface Element {
-		onclickdown: (listener: Listener<Element>) => void
+		/**
+		 * Clickdown creates "pointerdown", "keydown", and "click" listeners
+		 * to speed up actions. It toggles checkboxes and changes urls on "down".
+		 *
+		 * When no type is specified, the callback target will be of type Element.
+		 *
+		 * @param this - An Element
+		 * @param callback - This callback passes an event and target as parameters
+		 */
+		onclickdown: (listener: (event: PointerMouseKeyboard, target: Element) => void) => void
 	}
 
 	interface HTMLElement {
-		onclickdown: (listener: Listener<HTMLElement>) => void
+		/**
+		 * Clickdown creates "pointerdown", "keydown", and "click" listeners
+		 * to speed up actions. It toggles checkboxes and changes urls on "down".
+		 *
+		 * Uses HTMLElement with `getElementById`.
+		 *
+		 * @param this - An HTMLElement
+		 * @param callback - This callback passes an event and target as parameters
+		 */
+		onclickdown: (listener: (event: PointerMouseKeyboard, target: HTMLElement) => void) => void
 	}
 
 	interface HTMLInputElement {
-		onclickdown: (listener: Listener<HTMLInputElement>) => void
+		/**
+		 * Clickdown creates "pointerdown", "keydown", and "click" listeners
+		 * to speed up actions. It toggles checkboxes and changes urls on "down".
+		 *
+		 * You can specify the target as an input with `querySelector`, which exposes
+		 * .value or .checked methods.
+		 *
+		 * @param this - An HTMLInputElement
+		 * @param callback - This callback passes an event and target as parameters
+		 */
+		onclickdown: (listener: (event: PointerMouseKeyboard, target: HTMLInputElement) => void) => void
 	}
 }
 
 export interface Listener<T extends Element> {
-	(event: PointerEvent | MouseEvent | KeyboardEvent, target: T): void
+	(event: PointerMouseKeyboard, target: T): void
 }
